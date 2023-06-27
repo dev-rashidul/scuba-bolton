4e6ac0875fbd20ecbb61c5e514803b7f3b60b887
dev-rashidul <rashidul.contact@gmail.com>
2023-06-27 17:47:21 +0600

    More 3 pages done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,33 @@
 // JavaScript For Sticky Header
 
-window.addEventListener("scroll", function(){
-    const header = document.querySelector(".navbar");
-    header.classList.toggle("sticky", window.scrollY > 0)
-
-})
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".navbar");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
 // JavaScript For Mobile Menu
 
 const navMenu = document.querySelector(".nav-menu");
-const bars = document.getElementById("bars")
+const bars = document.getElementById("bars");
 
-bars.addEventListener("click", () =>{
-    navMenu.classList.toggle("active")
-})
+bars.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
+
+// Password Show Hide JS
+
+function passFunc() {
+  var passInput = document.getElementById("passInput");
+  var eyeHide = document.getElementById("eye-hide");
+  var eyeShow = document.getElementById("eye-show");
+
+  if (passInput.type === "password") {
+    passInput.type = "text";
+    eyeHide.style.display = "block";
+    eyeShow.style.display = "none";
+  } else {
+    passInput.type = "password";
+    eyeHide.style.display = "none";
+    eyeShow.style.display = "block";
+  }
+}
